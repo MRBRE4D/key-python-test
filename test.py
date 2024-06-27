a4a1baf0ce2a0565a25e7e0b656cbc593ed44d7a
bread <bread0114@m2x.com.tw>
2024-06-27 15:09:23 +0800

    init

---
+++ test.py
@@ -0,0 +1,67 @@
+import cv2
+import os 
+from google.cloud import vision_v1
+from google.cloud.vision_v1 import types
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "data\plasma-buckeye-427410-t8-6c2611019c9d.json"
+def draw_boxes(image_path, grouped_results):
+    image = cv2.imread(image_path)
+    for grouped_result in grouped_results:
+        bounding_poly = grouped_result.bounding_poly
+        x_min = bounding_poly.normalized_vertices[0].x * image.shape[1]
+        y_min = bounding_poly.normalized_vertices[0].y * image.shape[0]
+        x_max = bounding_poly.normalized_vertices[2].x * image.shape[1]
+        y_max = bounding_poly.normalized_vertices[2].y * image.shape[0]
+        cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
+
+        # 取得產品名稱和分數
+        for result in grouped_result.results:
+            product = result.product
+            product_display_name = product.display_name
+            product_score = result.score
+            
+            # 框線上顯示名稱和分數
+            #!  文字跟分數 改成results = response.product_search_results.results
+    
+            text = f"{product_display_name} ({product_score:.2f})"
+            cv2.putText(image, text, (int(x_min), int(y_min) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow('Image with Boxes', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def product_search(image_path):
+    image_annotator_client = vision_v1.ImageAnnotatorClient()
+    project_id = 'plasma-buckeye-427410-t8'
+    location = 'asia-east1'
+    product_set_id = '5e22bdcb197e2a53'
+    product_set_path = f'projects/{project_id}/locations/{location}/productSets/{product_set_id}'
+
+    with open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+    request = types.AnnotateImageRequest(
+        image=image,
+        features=[types.Feature(type=vision_v1.Feature.Type.PRODUCT_SEARCH)],
+        image_context=types.ImageContext(
+            product_search_params=types.ProductSearchParams(
+                product_set=product_set_path,
+                product_categories=['homegoods-v2']
+            )
+        )
+    )
+
+    response = image_annotator_client.annotate_image(request)
+    grouped_results = response.product_search_results.product_grouped_results
+    # boxes = []
+    # print("response=",response)
+    # for result in response.product_search_results.product_grouped_results:
+    #     boxes.append(result.bounding_poly)
+
+    return grouped_results
+
+if __name__ == "__main__":
+    image_path = "C:\\Users\\Bread\\Documents\\bread\\key-python-test\\data\\test\\S__24051720_0.jpg"
+    boxes = product_search(image_path)
+    draw_boxes(image_path, boxes)
---
+++ test2.py
@@ -0,0 +1,87 @@
+from google.cloud import vision
+import cv2
+import numpy as np
+
+import os 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "data\plasma-buckeye-427410-t8-6c2611019c9d.json"
+
+def get_similar_products_file(
+   project_id = 'plasma-buckeye-427410-t8',
+    location = 'asia-east1',
+    product_set_id = '5e22bdcb197e2a53',
+    product_category= 'homegoods-v2',
+    file_path= "C:\\Users\\Bread\\Documents\\bread\\key-python-test\\data\\test\\S__24051720_0.jpg",
+    filter= 'type=key' ,
+    max_results= 5,
+):
+    
+
+    # product_search_client is needed only for its helper methods.
+    product_search_client = vision.ProductSearchClient()
+    image_annotator_client = vision.ImageAnnotatorClient()
+
+    # Read the image as a stream of bytes.
+    with open(file_path, "rb") as image_file:
+        content = image_file.read()
+
+    # Create annotate image request along with product search feature.
+    image = vision.Image(content=content)
+
+    # product search specific parameters
+    product_set_path = product_search_client.product_set_path(
+        project=project_id, location=location, product_set=product_set_id
+    )
+    product_search_params = vision.ProductSearchParams(
+        product_set=product_set_path,
+        product_categories=[product_category],
+        filter=filter,
+    )
+    image_context = vision.ImageContext(product_search_params=product_search_params)
+
+    # Search products similar to the image.
+    response = image_annotator_client.product_search(
+        image, image_context=image_context, max_results=max_results
+    )
+
+    index_time = response.product_search_results.index_time
+    print("Product set index time: ")
+    print(index_time)
+
+    results = response.product_search_results.results
+    
+    print("Search results:")
+    for result in results:
+        product = result.product
+
+        print(f"Score(Confidence): {result.score}")
+        print(f"Image name: {result.image}")
+
+        print(f"Product name: {product.name}")
+        print("Product display name: {}".format(product.display_name))
+        print(f"Product description: {product.description}\n")
+        print(f"Product labels: {product.product_labels}\n")
+        
+        
+    grouped_results = response.product_search_results.product_grouped_results
+    
+    # 呈現圖片以及繪製框線
+    image = cv2.imread(file_path)
+    for grouped_result in grouped_results:
+        bounding_poly = grouped_result.bounding_poly
+        
+        x_min = bounding_poly.normalized_vertices[0].x * image.shape[1]
+        y_min = bounding_poly.normalized_vertices[0].y * image.shape[0]
+        x_max = bounding_poly.normalized_vertices[2].x * image.shape[1]
+        y_max = bounding_poly.normalized_vertices[2].y * image.shape[0]
+        
+        cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
+        
+        # 使用 response.product_search_results.results 中的 display_name 和 score
+        for result in results:
+            product = result.product
+            cv2.putText(image, f"{product.display_name} ({result.score:.2f})", (int(x_min), int(y_min) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            
+    cv2.imshow('Search Results', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+get_similar_products_file()
